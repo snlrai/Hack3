2f18b2dd4ee58a7b4b47145dfdb5d7f2b4de3865
bilal ahmed <yoissbilal@gmail.com>
2023-12-09 03:28:14 +0530

    first real

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -121,7 +121,7 @@ watch(
                 <div class="flex justify-content-between mb-3">
                     <div>
                         <span class="block text-500 font-medium mb-3">Orders</span>
-                        <div class="text-900 font-medium text-xl">152</div>
+                        <div class="text-900 font-medium text-xl">[]</div>
                     </div>
                     <div class="flex align-items-center justify-content-center bg-blue-100 border-round" style="width: 2.5rem; height: 2.5rem">
                         <i class="pi pi-shopping-cart text-blue-500 text-xl"></i>
@@ -136,6 +136,7 @@ watch(
                 <div class="flex justify-content-between mb-3">
                     <div>
                         <span class="block text-500 font-medium mb-3">Revenue</span>
+                        <div class="text-900 font-medium text-xl">₹2.100</div>
                         <div class="text-900 font-medium text-xl">₹2.100</div>
                     </div>
                     <div class="flex align-items-center justify-content-center bg-orange-100 border-round" style="width: 2.5rem; height: 2.5rem">
